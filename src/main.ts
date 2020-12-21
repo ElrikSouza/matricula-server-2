75cc27d0aaa451a1f11ffed30500a08f48ac6f9a
ElrikSouza <72892051+ElrikSouza@users.noreply.github.com>
2020-12-21 17:12:06 -0400

    added app's configuration to the main.ts file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
+import * as Env from './env';
+import { fastifyHelmet } from 'fastify-helmet';
+import compress from 'fastify-compress';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+    { cors: true },
+  );
+
+  app.register(fastifyHelmet);
+  app.register(compress, { encodings: ['gzip', 'deflate'] });
+  await app.listen(Env.PORT);
 }
+
 bootstrap();
