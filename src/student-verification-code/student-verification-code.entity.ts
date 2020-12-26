5a54d551839446d22537c5dfe98b2f83968f91a6
ElrikSouza <72892051+ElrikSouza@users.noreply.github.com>
2020-12-26 16:36:18 -0400

    added entity decorator to the class StudentVerificationCode

--- src/student-verification-code/student-verification-code.entity.ts
+++ src/student-verification-code/student-verification-code.entity.ts
@@ -1,5 +1,6 @@
-import { Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity()
 export class StudentVerificationCode {
   @PrimaryGeneratedColumn('increment')
   _id: number;
