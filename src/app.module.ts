500773c7c723254a139e53907ad0d61cd0b76ede
ElrikSouza <72892051+ElrikSouza@users.noreply.github.com>
2020-12-27 16:21:29 -0400

    updated app module definition

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
+import { StudentVerificationCodeModule } from './student-verification-code/student-verification-code.module';
+import { UserAccountModule } from './user-account/user-account.module';
+import * as Env from './env';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    AuthModule,
+    StudentVerificationCodeModule,
+    UserAccountModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: Env.DB_CONN,
+      autoLoadEntities: true,
+    }),
+  ],
 })
 export class AppModule {}
