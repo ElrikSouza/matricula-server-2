bb22d7de6e04a704e020100d6ef03711ea489eab
ElrikSouza <72892051+ElrikSouza@users.noreply.github.com>
2021-01-04 18:18:57 -0400

    fixed wrong queries in courses.service.ts

--- src/courses/courses.service.ts
+++ src/courses/courses.service.ts
@@ -13,16 +13,21 @@ export class CoursesService {
     const cachedResult = await this.cacheManager.get('courses');
 
     if (!cachedResult) {
-      const dbResult = await this.entityManager.query(
-        `select 
-        c._id, c.course_name, array_remove(array_agg(cp.prerequisite_id), NULL) as prerequisites
-        from course c left join course_prerequisite cp 
-        on cp.course_id = c._id group by c._id;`,
+      const dbResult = await this.entityManager.query(`
+      select
+         a_course.code, a_course.name,
+        array_remove(array_agg(co.code), NULL) as prerequisites
+      from course a_course left join (
+        course_prerequisite co_p inner join course co on co_p.prerequisite_id = co._id
+      ) on a_course._id = co_p.course_id group by a_course._id;`);
+
+      await this.cacheManager.set(
+        'courses',
+        { courses: dbResult },
+        { ttl: 3000000 },
       );
 
-      await this.cacheManager.set('courses', dbResult, { ttl: 3000000 });
-
-      return dbResult;
+      return { courses: dbResult };
     }
 
     return cachedResult;
@@ -32,19 +37,22 @@ export class CoursesService {
     const cachedResult = await this.cacheManager.get(`courses#${studentId}`);
 
     if (!cachedResult) {
-      const { coursecodes: courseCodes } = await this.entityManager.query(`
+      const [dbResult] = await this.entityManager.query(
+        `
         select
-        array_agg(course.code) as courseCodes from course course
-        inner join course_prerequisite course_p on course_p.course_id = course._id;
-      `);
-
-      await this.cacheManager.set(
-        `courses#${studentId}`,
-        { courseCodes },
-        { ttl: 300000 },
+        array_agg(co.code) as "courseCodes"
+        from course co inner join student_course_record scr
+        on co._id = scr.course_id 
+        where scr.student_id = $1;
+      `,
+        [studentId],
       );
 
-      return { courseCodes };
+      await this.cacheManager.set(`courses#${studentId}`, dbResult, {
+        ttl: 300000,
+      });
+
+      return dbResult;
     }
 
     return cachedResult;
