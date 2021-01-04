028738b7671773d21507379f6a88e11d3f1cef4e
ElrikSouza <72892051+ElrikSouza@users.noreply.github.com>
2021-01-04 18:22:58 -0400

    added missing typeorm module import to the EnrollmentRequestsModule

--- src/enrollment-requests/enrollment-requests.module.ts
+++ src/enrollment-requests/enrollment-requests.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { EnrollmentRequest } from './enrollment-request.entity';
 import { EnrollmentRequestsController } from './enrollment-requests.controller';
 import { EnrollmentRequestsService } from './enrollment-requests.service';
 
 @Module({
   controllers: [EnrollmentRequestsController],
   providers: [EnrollmentRequestsService],
+  imports: [TypeOrmModule.forFeature([EnrollmentRequest])],
 })
 export class EnrollmentRequestsModule {}
