aac03f54ecafd3f2273ec0bece428f5be8c04d6e
ElrikSouza <72892051+ElrikSouza@users.noreply.github.com>
2021-01-04 18:18:24 -0400

    fixed wrong payload property names in jwt.strategy.ts

--- src/auth/jwt/jwt.strategy.ts
+++ src/auth/jwt/jwt.strategy.ts
@@ -14,6 +14,6 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: any) {
-    return { userId: payload._id, studentId: payload.student_id };
+    return { userId: payload.userId, studentId: payload.studentId };
   }
 }
