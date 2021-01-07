d54acb985921cc5f23b21d1beca92760449cce18
ElrikSouza <72892051+ElrikSouza@users.noreply.github.com>
2021-01-07 18:15:29 -0400

    use the course code as the primary key, removed the _id serial primary key

--- schema.sql
+++ schema.sql
@@ -18,30 +18,29 @@ create table student_verification_code(
 );
 
 create table course(
-    _id serial primary key,
-    code varchar(6) unique,
+    code char(6) primary key,
     name varchar(70)
 );
 
-create table course_prerequisite(
+create table course_prerequisite (
     _id serial primary key,
-    course_id int not null references course(_id) on delete cascade,
-    prerequisite_id int not null references course(_id) on delete cascade
+    course_code char(6) not null references course(code) on delete cascade,
+    prerequisite_code char(6) not null references course(code) on delete cascade
 );
 
 create table student_course_record(
     _id serial primary key,
     student_id int not null references student(_id) on delete cascade,
-    course_id int not null references course(_id) on delete cascade
+    course_code char(6) not null references course(code) on delete cascade
 );
 
-create unique index student_course_record_index on student_course_record(student_id, course_id);
+create unique index student_course_record_index on student_course_record(student_id, course_code);
 
 create table enrollment_request(
     _id serial primary key,
     student_id int not null references student(_id) on delete cascade,
-    course_id int not null references course(_id) on delete cascade,
+    course_code char(6) not null references course(code) on delete cascade,
     created_at timestamptz not null default now()
 );
 
-create unique index enrollment_request_index on enrollment_request(student_id, course_id);
+create unique index enrollment_request_index on enrollment_request(student_id, course_code);
